f083555b6cbca31e5de8ee47e44203bb212e0d3a
Ahmed MOUHI <amouhi@gmail.com>
2021-04-07 22:46:48 +0200

    Adding logging level comment

--- dbot.py
+++ dbot.py
@@ -2,6 +2,8 @@ import discord
 import sys
 import os
 import logging
+
+# logging level
 logging.basicConfig(level=logging.DEBUG)
 
 logging.debug('Starting Application')
@@ -21,18 +23,22 @@ join_greeting = '''Welcome to the DevOps Journey Discord! We are a new community
 YouTube Channel: https://www.youtube.com/channel/UC4Snw5yrSDMXys31I18U3gg
 '''
 
+
 class MyClient(discord.Client):
     async def on_ready(self):
         print('Logged on as {0}!'.format(self.user))
 
     async def on_message(self, message):
         print('Message from {0.author}: {0.content}'.format(message))
-        if message.author == client.user: return
-        if message.content == '!stop': await client.logout()
-        #if message.content.startswith('!ping'): await message.channel.send('Pong!')
-    
+        if message.author == client.user:
+            return
+        if message.content == '!stop':
+            await client.logout()
+        # if message.content.startswith('!ping'): await message.channel.send('Pong!')
+
     async def on_member_join(self, member):
         await member.send(join_greeting)
+
 
 client = MyClient()
 client.run(token)
